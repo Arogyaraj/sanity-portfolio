ab90f82a92ee85458d29a3cd68f531c11c7bc176
Arogya Raj De Joseph <Arogyaraj@users.noreply.github.com>
2019-12-28 19:32:54 +0530

    Create instagramPost.js

---
+++ studio/schemas/objects/instagramPost.js
@@ -0,0 +1,12 @@
+export default {
+  type: "object",
+  name: "instagramPost",
+  title: "Instagram Post",
+  fields: [
+    {
+      name: "url",
+      type: "url",
+      description: "Visit an Instagram post in a browser and copy the URL."
+    }
+  ]
+};
